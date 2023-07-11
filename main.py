aa531bd9bb3c259082581a02b66f1cd22e8db03c
iambased789 <126959732+iambased789@users.noreply.github.com>
2023-07-11 22:34:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,6 +123,8 @@ with textcontainer:
             conversation_string = get_conversation_string()
             st.code(conversation_string)
             refined_query = query_refiner(conversation_string, query)
+            st.subheader("Refined Query:")
+            st.write(refined_query)
             context = find_match(refined_query)
             response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
         st.session_state.requests.append(query)
