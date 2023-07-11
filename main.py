202eb8efa3c8c9b768f76bfd3101c42f02a4200c
iambased789 <126959732+iambased789@users.noreply.github.com>
2023-07-11 21:43:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,6 @@ headers = {
 
 st.subheader("ChanakyaGPT")
 
-openai.api_key = st.secrets['path']
 ####################################################
 
 pinecone.init(api_key=PINECONE_API_KEY,environment='us-west1-gcp-free' )
